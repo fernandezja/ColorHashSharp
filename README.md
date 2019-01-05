129c152dbe1c590927966a1634ce1085749e47b5
Jose A. Fernandez <fernandezja@gmail.com>
2019-01-05 18:57:33 -0300

    Code example for readme with test

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# color-hash
+﻿# color-hash
 Generate color based on the given string. C# port of [ColorHash Javascript Library](https://github.com/zenozeng/color-hash).
 
 
 [![Build status](https://fernandezja.visualstudio.com/color-hash/_apis/build/status/color-hash-CI)](https://fernandezja.visualstudio.com/color-hash/_build/latest?definitionId=4)
+
+
+#### Basic
+
+```csharp
+var colorHash = new ColorHash();
+
+// in HSL, Hue ∈ [0, 360), Saturation ∈ [0, 1], Lightness ∈ [0, 1]
+colorHash.hsl('Hello World'); // [ 225, 0.65, 0.35 ]
+
+// in RGB, R, G, B ∈ [0, 255]
+colorHash.rgb('Hello World'); // [ 135, 150, 197 ]
+
+// in HEX
+colorHash.hex('Hello World'); // '#8796c5'
+```
---
+++ src/ColorHash.Tests/ReadmeCodeTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ColorHashSharp = Fernandezja.ColorHash;
+using Xunit;
+using System.Diagnostics;
+
+namespace ColorHash.Tests
+{
+    public class ReadmeCodeTest
+    {
+        [Fact(DisplayName = "ReadmeCodeExample_ToCopyPaste")]
+        public void ReadmeCodeExample_ToCopyPaste()
+        {
+            Debug.WriteLine($"ColorHash >  Hello World");
+
+            var colorHash = new ColorHashSharp.ColorHash();
+
+            //HSL (return HSL object)
+            var hsl = colorHash.Hsl("Hello World");
+
+            Debug.WriteLine($"  H = {hsl.H} | S = {hsl.S} | L = {hsl.L}");
+
+
+            //RGB (return System.Drawing.Color object)
+            var color = colorHash.Rgb("Hello World");
+
+            Debug.WriteLine($"   R= {color.R} | G = {color.G} | B = {color.B}");
+            
+
+            //Hex (return string)
+            var hex = colorHash.Hex("Hello World");
+
+            Debug.WriteLine($"  hex = {hex}");
+        }
+    }
+}
